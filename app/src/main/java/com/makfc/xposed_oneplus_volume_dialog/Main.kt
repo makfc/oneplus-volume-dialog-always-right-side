e4dc3c1cbf7dee8edd80741cbdfd156b8c783ea5
makfc <francis.mak.fc@gmail.com>
2021-11-14 21:26:23 +0800

    feat: add disable lockscreen media art

--- app/src/main/java/com/makfc/xposed_oneplus_volume_dialog/Main.kt
+++ app/src/main/java/com/makfc/xposed_oneplus_volume_dialog/Main.kt
@@ -1,10 +1,16 @@
 package com.makfc.xposed_oneplus_volume_dialog
 
-import de.robv.android.xposed.*
+import android.graphics.Bitmap
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedBridge
 import de.robv.android.xposed.XposedHelpers.findAndHookMethod
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
 
 class Main : IXposedHookLoadPackage {
+    private val CLASS_NOTIF_MEDIA_MANAGER =
+        "com.android.systemui.statusbar.NotificationMediaManager"
+
     fun log(text: String) {
         XposedBridge.log("${BuildConfig.APPLICATION_ID}: $text")
     }
@@ -37,6 +43,21 @@ class Main : IXposedHookLoadPackage {
                 findAndHookMethod(
                     "com.oneplus.volume.OpOutputChooserDialog", lpparam.classLoader,
                     "isLandscape", methodHookReturnTrue
+                )
+
+                // disable lockscreen media art
+                findAndHookMethod(
+                    CLASS_NOTIF_MEDIA_MANAGER,
+                    lpparam.classLoader,
+                    "finishUpdateMediaMetaData",
+                    Boolean::class.javaPrimitiveType,
+                    Boolean::class.javaPrimitiveType,
+                    Bitmap::class.java,
+                    object : XC_MethodHook() {
+                        override fun beforeHookedMethod(param: MethodHookParam) {
+                            param.args[2] = null
+                        }
+                    }
                 )
             }
         }
